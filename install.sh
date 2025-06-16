ca2a41414b1832394840a0ea43c648d91ed56642
Bruno Braga <brunobragaw8t@gmail.com>
2025-06-16 11:57:03 +0100

    feat(alacritty): hide title bar

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ DOTFILES_DIR="$HOME/dotfiles"
 [ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.backup
 ln -s "$DOTFILES_DIR/git/.gitconfig" ~/.gitconfig
 
+mkdir ~/.config/alacritty
+[ -f ~/.config/alacritty/alacritty.toml ] && mv ~/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml.backup
+ln -s "$DOTFILES_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
+
 sudo ln -s "$DOTFILES_DIR/bash/.bashrc" /etc/bashrc.local
 
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.backup

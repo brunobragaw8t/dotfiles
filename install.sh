db71862817f3d7bd1fa29ea2344e499f9aecd5ea
Bruno Braga <brunobragaw8t@gmail.com>
2025-06-16 09:58:32 +0100

    fix(bash): change symlink path

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ DOTFILES_DIR="$HOME/dotfiles"
 [ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.backup
 ln -s "$DOTFILES_DIR/git/.gitconfig" ~/.gitconfig
 
-[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.backup
-ln -s "$DOTFILES_DIR/bash/.bashrc" ~/.bashrc
+sudo ln -s "$DOTFILES_DIR/bash/.bashrc" /etc/bashrc.local
 
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.backup
 ln -s "$DOTFILES_DIR/tmux/.tmux.conf" ~/.tmux.conf

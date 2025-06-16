ef935bfb7a566fc2147e057f839eebfb731cc20f
Bruno Braga <brunobragaw8t@gmail.com>
2025-06-16 09:41:26 +0100

    feat(cursor): add user settings, keybindings and installation script

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ ln -s "$DOTFILES_DIR/bash/.bashrc" ~/.bashrc
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.backup
 ln -s "$DOTFILES_DIR/tmux/.tmux.conf" ~/.tmux.conf
 
+[ -f ~/.config/Cursor/User/settings.json ] && mv ~/.config/Cursor/User/settings.json ~/.config/Cursor/User/settings.json.backup
+ln -s "$DOTFILES_DIR/cursor/user-settings.json" ~/.config/Cursor/User/settings.json
+
+[ -f ~/.config/Cursor/User/keybindings.json ] && mv ~/.config/Cursor/User/keybindings.json ~/.config/Cursor/User/keybindings.json.backup
+ln -s "$DOTFILES_DIR/cursor/user-keybindings.json" ~/.config/Cursor/User/keybindings.json

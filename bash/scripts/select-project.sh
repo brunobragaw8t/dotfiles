3b547010105428ab798c82f01a04805530d50246
Bruno Braga <brunobragaw8t@gmail.com>
2026-01-06 10:38:15 +0000

    feat(bash): add split to dev script and open apps automatically

--- bash/scripts/select-project.sh
+++ bash/scripts/select-project.sh
@@ -33,10 +33,12 @@ selected_path="$HOME/projects/$selected_dir"
 
 tmux split-window -h
 tmux split-window -h
+tmux split-window -h
 tmux select-layout even-horizontal
 
-tmux send-keys -t 0 "cd '$selected_path' && clear" C-m
-tmux send-keys -t 1 "cd '$selected_path' && clear" C-m
-tmux send-keys -t 2 "cd '$selected_path' && clear" C-m
+tmux send-keys -t 0 "cd '$selected_path' && lazygit" C-m
+tmux send-keys -t 1 "cd '$selected_path' && opencode" C-m
+tmux send-keys -t 2 "cd '$selected_path' && vim" C-m
+tmux send-keys -t 3 "cd '$selected_path' && clear" C-m # Left empty to run dev server
 
 tmux rename-window "$selected_dir"
